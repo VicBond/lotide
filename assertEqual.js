351798ebe1216a90af06c2faa10ad5640a143680
Victor Bondarenko <nirasimha@gmail.com>
2021-10-25 19:46:52 +0000

    Self made console.assert

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) =>{
+  if (actual !== expected) {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 20);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
