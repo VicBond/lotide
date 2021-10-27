94e3b50714585fbd1ce5547d0a7532d521d33c44
Victor Bondarenko <nirasimha@gmail.com>
2021-10-27 18:52:05 +0000

    CountOnly function added

---
+++ countOnly.js
@@ -0,0 +1,55 @@
+// ASSERT EQUAL
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+
+const countOnly = (allItems, itemsToCount) => {
+  const result = {};
+for (let item of allItems){
+  if (itemsToCount[item]){
+  if (result[item]){
+    result[item] += 1;
+  } else {
+    result[item] = 1;
+  }
+}
+}
+
+
+console.log(result);
+return result;
+
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const inTheList = { 
+  "Jason": true,
+  "Karima": true,
+  "Fang": true,
+  "Agouhanna": false,
+}
+
+const result1 = countOnly(firstNames, inTheList);
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
