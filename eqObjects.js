32c76eee4e0147a0ffc628feaecb5d11d90d0f95
Victor Bondarenko <nirasimha@gmail.com>
2021-10-28 02:05:18 +0000

    Function camparing Objects added

---
+++ eqObjects.js
@@ -0,0 +1,62 @@
+
+const assertEqual = (actual, expected) =>{
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const eqArrays = (index1, index2) => {
+ 
+  if (index1.length !== index2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < index1.length; i++) {
+      if (index1[i] !== index2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+// TEST CODE
+
+
+const eqObjects = function(object1, object2) {
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if ((array1.length) === (array2.length)) {
+    for (let key of array1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba);  // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+
+// TEST CODE
+
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
